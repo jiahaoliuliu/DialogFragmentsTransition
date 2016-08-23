35b9795491f54ca24296ba874634455bfad56465
Jiahao Liu <jiahaoliuliu@gmail.com>
2016-08-23 17:34:40 +0400

    Added listener to the button

--- app/src/main/java/com/jiahaoliuliu/dialogfragmentstransition/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/dialogfragmentstransition/MainActivity.java
@@ -2,12 +2,38 @@ package com.jiahaoliuliu.dialogfragmentstransition;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private Button mShowDialogFragmentsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Link the views
+        mShowDialogFragmentsButton = (Button) findViewById(R.id.show_dialogs_button);
+        mShowDialogFragmentsButton.setOnClickListener(mOnClickListener);
+    }
+
+    private View.OnClickListener mOnClickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.show_dialogs_button:
+                    showDialogs();
+                    break;
+            }
+        }
+    };
+
+    private void showDialogs() {
+        Log.v(TAG, "Showing dialogs");
     }
 }
